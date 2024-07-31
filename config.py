a66bac1e62e3e18271d86498b3c4a5e5a962abcc
TgmBet <tgm.bet@outlook.com>
2024-08-01 02:15:41 +0800

    ss

--- config.py
+++ config.py
@@ -2,12 +2,12 @@ TGM_TOKEN = '7031359380:AAG9QzrH03ZzMgitxkcGrk26BMUDySTsOxo'#'7394333754:AAG7QED
 DATABASE_URL = ""
 DATABASE_PASSWORD = ""
 DATABASE_USERNAME = ""
-ANIMATION_GIF = "https://i.imgur.com/v5EEofM.mp4" #"TGM.gif"
-TGM_EMBED_IMAGE = '<a href="https://i.ibb.co/hLnpPC1/Base-LOGO.png"> </a>'
+ANIMATION_GIF = "https://raw.githubusercontent.com/domi-finance/sss/main/TGM.gif" #"TGM.gif"
+TGM_EMBED_IMAGE = '<a href="https://raw.githubusercontent.com/domi-finance/sss/main/Base_LOGO.png"> </a>'
 TGM_WEBAPP = "https://github.com/tgm-labs/telegram-apps"
 TGM_CHINESE_CHAT = 'https://t.me/+Qi-cZ5yBlhFjNmVk'
 TGM_ENGLISH_CHAT = 'https://t.me/+U5baYNOiCqNhZjZk'
-SUPPORT_CHANNEL_ID = -1002236707181#'https://t.me/+Mu8YNYDZJEs5ODk0'
+SUPPORT_CHANNEL_ID = 'https://t.me/+n0ZtKOz143Y0MTU1'
 
 # TGM_EMBED_IMAGE = '<a href="https://i.ibb.co/P4M3FpY/try-fake-2.png">&#8205;</a>'
 #<a href="https://ibb.co/CBTf0XK"><img src="https://i.ibb.co/H7kjgMT/TGM.gif" alt="TGM" border="0"></a>
